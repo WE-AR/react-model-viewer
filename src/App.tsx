0684045b9151d7b8792cd1db3c5faa76cbb36583
Green <shren0812@gmail.com>
2023-11-15 15:25:57 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 export default function App() {
   const modelRef = useRef(null);
-  const [isPlaying, setPlaying] = useState(false);
 
   useEffect(() => {
     const checkARAvailability = setInterval(() => {
