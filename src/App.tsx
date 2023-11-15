bdfbfd58c7a2f738ef766d8d0540f667f1d14717
Green <shren0812@gmail.com>
2023-11-15 14:46:49 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import "./App.css";
 
 export default function App() {
+  const modelRef = useRef(null);
+
+  useEffect(() => {
+    if (modelRef.current) {
+      const model = modelRef.current;
+
+      if (model["canActivateAR"]) {
+        // @ts-ignore
+        model["activateAR"]();
+      }
+    }
+
+  },[])
+
+
   return (
     <div className='App'>
       <model-viewer
+        ref={modelRef}
         ar-modes="webxr scene-viewer quick-look"
         style={{width: '100%', height: '100%'}}
         environment-image="neutral"
